5250dd391d8be4201a008c98143a4322796432b6
Kay Brodersen <khbrodersen@gmail.com>
2020-12-22 19:15:56 +0100

    Initial Commit

---
+++ Dispersion/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Dispersion
+//
+//  Created by Kay H. Brodersen on 22/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
